b3ce05c5d2441c9ff798b5cc9b2bbbf9e4344637
stonesaw <mail.sou.dev@gmail.com>
2022-08-29 15:28:46 +0900

    add: cat

---
+++ fixtures/main_game/cat.rb
@@ -0,0 +1,28 @@
+module Fixture::MainGame
+  class Cat < Sprite
+    def initialize(x, y, image)
+      self.x, self.y, self.image = x, y, image
+      @dx = 4
+      @dy = 4
+    end
+
+    def update
+      self.x += @dx
+      self.y += @dy
+      if self.x < 0
+        self.x = 0
+        @dx = - @dx
+      elsif self.x + self.image.width > Window.width
+        self.x = Window.width - self.image.width
+        @dx = - @dx
+      end
+      if self.y < 0
+        self.y = 0
+        @dy = - @dy
+      elsif self.y + self.image.height > Window.height
+        self.y = Window.height - self.image.height
+        @dy = - @dy
+      end
+    end
+  end
+end
