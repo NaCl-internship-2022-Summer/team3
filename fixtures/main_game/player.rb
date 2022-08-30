d270a7133b8d400f39dd4f16f52610f499151641
sachisuzuki <suzuki@netlab.jp>
2022-08-30 13:38:52 +0900

    Player が向いた方向に画像の顔が向くように修正
    	modified:   fixtures/main_game/player.rb
    	new file:   images/_original_videographer.png
    	new file:   images/left_videographer.png
    	renamed:    images/videographer.png -> images/right_videographer.png

--- fixtures/main_game/player.rb
+++ fixtures/main_game/player.rb
@@ -1,5 +1,7 @@
 module Fixture::MainGame
   class Player < Sprite
+    attr_reader :direction
+
     def initialize(x, y)
       # self.x, self.y: Spriteを親に持つPlayerはattr_accessorで定義されたx, yを持つ
 
@@ -7,13 +9,20 @@ module Fixture::MainGame
       # self.変数: selfの持つ変数を呼び出す (その処理はgetter/setterを呼び出す)
       # @変数:     インスタンス変数 (privateで参照可能な変数)
 
+      @right_player = Image.load("images/right_videographer.png")
+      @left_player = Image.load("images/left_videographer.png")
+
       self.x = x
       self.y = y
-      self.image = Image.load("images/videographer.png")
+      self.image = @right_player
       self.image.set_color_key(C_WHITE)
+      self.center_x = 40
+      self.center_y = 34
+      self.image.circle_fill(self.center_x, self.center_y, 5, C_BLACK)
     end
 
     def update
+
       if Input.key_down?(K_W) || Input.key_down?(K_UP)
         self.y -= 1
       elsif Input.key_down?(K_A) || Input.key_down?(K_LEFT)
@@ -26,11 +35,18 @@ module Fixture::MainGame
       self.x = [0, [self.x, Window.width - self.image.width].min].max
       self.y = [0, [self.y, Window.height - self.image.height].min].max
 
-      mouse_pos_x = Input.mouse_x - self.x
-      mouse_pos_y = Input.mouse_y - self.y
+      center_x = self.x + self.image.width / 2
+      center_y = self.y + self.image.height / 2
+      mouse_pos_x = Input.mouse_x - center_x
+      mouse_pos_y = Input.mouse_y - center_y
       radian = Math.atan2(mouse_pos_y, mouse_pos_x)
-      direction = 180 / Math::PI * radian
-      self.angle = direction
+      @direction = 180 / Math::PI * radian
+
+      if center_x < Input.mouse_x
+        self.image = @right_player
+      else
+        self.image = @left_player
+      end
     end
   end
 end
